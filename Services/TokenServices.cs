6aa9346c81c984dbea205809a6e026e394bbd6db
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-08 08:40:24 -0300

    Gerando Token

--- Services/TokenServices.cs
+++ Services/TokenServices.cs
@@ -10,7 +10,11 @@ namespace Blog.Services
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-            var tokenDescriptor = new SecurityTokenDescriptor(); // Item que vai conter todas as informações do token.
+            var tokenDescriptor = new SecurityTokenDescriptor     // Item que vai conter todas as informações do token.
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
             var token = tokenHandler.CreateToken(tokenDescriptor); // item que vai criar o token
             return tokenHandler.WriteToken(token); // vai retornar uma string
         }
