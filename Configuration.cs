2054c88c677483eab0bf7986046fb643fb3dcf00
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-12 22:15:53 -0300

    Implementando um ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,8 @@ namespace Blog
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "ZmVkYwY3ZDg4NjNiNDhlMTk3YjkyODdkNDKyYjcwOGU=";
+        public static string JwtKey = "ZmVkYwY3ZDg4NjNiNDhlMTk3YjkyODdkNDKyYjcwOGU=";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Blog.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
